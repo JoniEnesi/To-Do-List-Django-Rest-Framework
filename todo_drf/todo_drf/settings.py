b5eec0812574ffe2083fcf12455d3d90af1a4546
Joni <jonienesi10@gmail.com>
2024-06-13 13:40:55 +0200

    .

--- todo_drf/todo_drf/settings.py
+++ todo_drf/todo_drf/settings.py
@@ -130,4 +130,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'jonienesi10@gmail.com'
+EMAIL_HOST_PASSWORD = 'saep ykqf fbzn ounw'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- todo_drf/user/urls.py
+++ todo_drf/user/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 from .views import CustomLoginView, RegisterView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', CustomLoginView.as_view(), name='login'),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('register/', RegisterView.as_view(), name='register'),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="registration/reset_password.html"), name='reset_password'),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/reset_password_sent.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/reset.html"), name='password_reset_confirm'),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/reset_password_complete.html"), name='password_reset_complete'),
 ]
